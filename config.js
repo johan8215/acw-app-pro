fb9431c025e3bde7f136bc369355ae34b9957ad4
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-20 14:18:24 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,10 @@
+/* ===========================================================
+   ACW-App v4.6.5 — Direct Login (Blue Glass Edition)
+   Configuración principal
+   =========================================================== */
+
 const CONFIG = {
-  APP_VERSION: "v4.6.5 Blue Glass",
-  BASE_URL: "https://script.google.com/macros/s/AKfycbyY55LeXxMtlrNovUtnLKrEblrel6ljRnBjOpB2qWNSUnyMPLmWTuzScqSYGStKZ-2l/exec",
+  APP_VERSION: "v4.6.5 — Blue Glass Edition",
+  BASE_URL: "https://script.google.com/macros/s/AKfycbwn1uvIg-b7r69nGqvpY4NdfLHZ9av5vbOtsudQ8Fhrlx4Hb4EkEUcXY9r1mHLJmrjE/exec",
   LANG_DEFAULT: "en"
 };
