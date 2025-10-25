7df3adde4691d803684dc6c97ac226b512649c8c
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-25 15:44:50 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 const CONFIG = {
-  BASE_URL: "https://script.google.com/macros/s/AKfycbwgwpnpeB9ZUxn241xITDlsTNSOdiDqNqh0fWpfX7QCiAPGjEWwTfnDD4si88fIEI7O/exec",
+  BASE_URL: "https://script.google.com/macros/s/AKfycbyeMyHywFBLOCC98XyZxhd5NZ4-k6gfqN_Is8UI1GsL7e5UvTm7usCiG4OgE4grGoM-/exec",
   VERSION: "v4.6.9 — Stable Blue Glass Edition"
 };
 
