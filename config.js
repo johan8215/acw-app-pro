555c65c64794b2d2b81aa441e7f9665bdfcf43f3
johan8215 <52975608+johan8215@users.noreply.github.com>
2025-10-25 22:42:44 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,5 @@ const CONFIG = {
   BASE_URL: "https://script.google.com/macros/s/AKfycbx-6DqfjydMMGp-K2z8FeBSH9t8Z1Ooa0Ene0u917RK7Eo6vu80aOTLmCf7lJtm-Ckh/exec",
   VERSION: "v4.6.9 — Stable Blue Glass Edition"
 };
+
 console.log(`✅ ACW-App connected → ${CONFIG.VERSION}`);
